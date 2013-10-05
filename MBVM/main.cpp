3db6d6f81c133e9690d96cf01c6d509c4f86b1fd
ogsh <nct.ogsh@gmail.com>
2013-10-05 12:36:23 +0900

    Initial Commit

---
+++ MBVM/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MBVM
+//
+//  Created by ogsh on 2013/10/05.
+//  Copyright (c) 2013年 ogsh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
